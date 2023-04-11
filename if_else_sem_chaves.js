ad648b95a09f5ccb28d7cede00f6f83352deb75d
Joe <joedsonfera@hotmail.com>
2023-04-11 16:10:48 -0300

    sixth commit

---
+++ if_else_sem_chaves.js
@@ -0,0 +1,10 @@
+// sem chaves o comando funciona apenas para uma linha;
+
+const idade = 29;
+
+if (idade >= 18)
+    console.log("É maior de idade");
+else
+    console.log("É menor de idade");
+
+console.log("Fim do código");
