2ebe4b868b5ba55324ea4fa00feb6b243522d262
Joe <joedsonfera@hotmail.com>
2023-04-11 18:24:03 -0300

    fourteenth commit

---
+++ exercicio1_condicionais_dds_t12.js
@@ -0,0 +1,14 @@
+const valorDaCompra = 100;
+const numeroDeParcelas = 2;
+
+if (numeroDeParcelas === 1) {
+    // a vista - com 10% de desconto
+    const desconto = valorDaCompra * 10 / 100;
+    const valorAPagar = valorDaCompra - desconto;
+    console.log(`Você deve pagar R$ ${valorAPagar}, pois à vista tem 10% de desconto`)
+} else {
+    // parcelado
+    const valorDaParcela = valorDaCompra / numeroDeParcelas;
+    console.log(`Você deve pagar R$ ${numeroDeParcelas} parcelas de R$ ${valorDaParcela}`)
+
+}
