4810683374456cf07b7030c49463fee6db218364
Joe <joedsonfera@hotmail.com>
2023-04-11 15:08:03 -0300

    first commit

---
+++ operadores-comparacao.js
@@ -0,0 +1,16 @@
+const ehMaior = 25 > 16;
+console.log(ehMaior);
+
+const ehmaior = 15 > 16;
+console.log(ehmaior);
+
+const eHmaior = 16 >= 16;
+console.log(eHmaior);
+
+const ehmAior = 16 <= 16;
+console.log(ehmAior);
+
+const x = 8;
+const y = 16;
+const ehmaIor = x <= y;
+console.log(ehmaIor);
