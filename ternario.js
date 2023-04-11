a67ce36aa80be9a472a4ccf022751bcbc56ba16f
Joe <joedsonfera@hotmail.com>
2023-04-11 18:04:04 -0300

    thirtiet commit

---
+++ ternario.js
@@ -0,0 +1,17 @@
+const saldo = 1;
+
+const unidade = (saldo === 1 ? "real" : "reais");
+
+console.log(`Você tem ${saldo} ${unidade}`);
+
+// if (saldo === 1) {
+//     unidade = "real"
+// } else {
+//     idade = "reais"
+// }
+
+// if (saldo === 1) {
+//     console.log(`Você tem ${saldo} real`)
+// } else {
+//     console.log(`Você tem ${saldo} reais`);
+// }
