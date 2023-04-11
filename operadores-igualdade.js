5aa576214ef260d1f0bb740899e9a4ba5fe3bc85
Joe <joedsonfera@hotmail.com>
2023-04-11 15:19:43 -0300

    second commit

---
+++ operadores-igualdade.js
@@ -0,0 +1,15 @@
+const ehIgual = (18 == 18);
+console.log(ehIgual);
+
+const ehigual = (18 == 19);
+console.log(ehigual);
+
+const eHigual = (19 != 18);
+console.log(eHigual);
+
+const ehiGual = (18 != 18);
+console.log(ehiGual);
+
+let x = 10;
+const ehigUal = (x != 18);
+console.log(ehigUal);
