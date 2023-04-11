570f5d6f737edbbc353549a8616c173f1c1c33cf
Joe <joedsonfera@hotmail.com>
2023-04-11 16:55:50 -0300

    eleventh commit

---
+++ negacao.js
@@ -0,0 +1,10 @@
+const temIngresso = false;
+
+// "!" inverte o valor da variável;
+
+if (!temIngresso) {
+    console.log("Pessoa sem ingresso.")
+} else {
+    console.log("Pessoa sem ingresso.")
+}
+console.log(`variavel temIngresso: ${temIngresso}`);
