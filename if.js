579f7b390575a48a2be77ccff2f7c7191d5eea2e
Joe <joedsonfera@hotmail.com>
2023-04-11 15:38:53 -0300

    fourth commit

---
+++ if.js
@@ -0,0 +1,13 @@
+const idade = 29;
+const iDade = 17;
+
+if (idade >= 18) {
+    // é executado caso a condição seja verdadeira;
+    console.log("É maior de idade");
+}
+
+if (iDade >= 18) {
+    console.log("É maior de idade");
+}
+
+console.log("Fim do código");
